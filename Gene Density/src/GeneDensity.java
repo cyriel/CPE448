77f22fc84b27318b9003be94a96c5e3390587197
Elton Yang <eyang03@calpoly.edu>
2014-10-14 14:14:55 -0700

    Added Nest logic functions to program

--- Gene Density/src/GeneDensity.java
+++ Gene Density/src/GeneDensity.java
@@ -16,16 +16,11 @@ column 10: name (use function to clean "" and ""; && everything after dash)
 
 public class GeneDensity {
 	public static String extractRecordDNA(String dna, Record rec) {
-		String extracted = null;
-		//some logic happens here
-		return rec.direction ? extracted : reverseComplement(extracted);
+		return rec.direction ? dna.substring(rec.start,rec.stop) : reverseComplement(dna.substring(rec.stop,rec.start));
 	}
 	
 	public static String extractGeneDNA(String dna, Isoform iso) {
-		String extracted = null;
-		if 
-		//some logic happens here
-		return iso.direction ? extracted : reverseComplement(extracted);
+		return iso.direction ? dna.substring(iso.start, iso.stop) : reverseComplement(dna.substring(iso.stop, iso.start));
 	}
 	
 	public static String reverseComplement(String dna) {
@@ -43,7 +38,83 @@ public class GeneDensity {
 		return reverse.toString();
 	}
 	
-//	nested genes: overlap start and stop
+	/* return values: 0: no nesting
+	 *                1: nested from iso1.start to iso2.stop
+	 *                2: nested from iso2.start to iso1.start
+	 *                3: iso1 is nested inside iso2
+	 *                4: nested from iso1.stop to iso2.stop
+	 *                5: nested from iso2.start to iso1.stop
+	 *                6: iso1 is nested inside iso2
+ 	 *                7: nested from iso2.start to iso1.stop
+	 *                8: nested from iso1.start to iso2.start
+	 *                9: iso2 is nested inside iso1
+	 *               10: nested from iso2.stop to iso1.stop
+	 *               11: nested from iso1.start to iso2.stop
+	 *               12: iso2 is nested inside iso1
+	 */
+	public int isNested(Isoform iso1, Isoform iso2) {
+	   if (iso1.start > iso2.start && iso1.start < iso2.stop)
+         if (iso1.stop > iso2.stop)                   // nested from iso1.start to iso2.stop
+            return 1;
+         else if (iso1.stop < iso2.start)             // nested from iso2.start to iso1.start
+            return 2;
+         else                                         // iso1 is nested inside iso2 <-- something went wrong?
+            return 3;
+      else if (iso1.stop > iso2.start && iso1.stop < iso2.stop)
+         if (iso1.start > iso2.stop)                  // nested from iso1.stop to iso2.stop
+            return 4;
+         else if (iso1.start < iso2.start)            // nested from iso2.start to iso1.stop
+            return 5;
+         else                                         // iso1 is nested inside iso2 <-- something went wrong?
+            return 6;
+      else if (iso2.start > iso1.start && iso2.start < iso1.stop)
+         if (iso2.stop > iso1.stop)                   // nested from iso2.start to iso1.stop
+            return 7;
+         else if (iso2.stop < iso1.start)             // nested from iso1.start to iso2.start
+            return 8;
+         else                                         // iso2 is nested inside iso1 <-- something went wrong?
+            return 9;
+      else if (iso2.stop > iso1.start && iso2.stop < iso1.stop)
+         if (iso2.start > iso1.stop)                  // nested from iso2.stop to iso1.stop
+            return 10;
+         else if (iso2.start < iso1.start)            // nested from iso1.start to iso2.stop
+            return 11;
+         else                                         // iso2 is nested inside iso1 <-- something went wrong?
+            return 12;
+      else
+         return 0;
+	}
+	
+	// hint should be return value from isNested
+	// return is [start,stop]
+	public int[] nested(Isoform iso1, Isoform iso2, int hint) {
+	   int[] range;
+	   if (hint == 1)
+	      range = new int[] {iso1.start,iso2.stop};
+	   else if (hint == 2)
+	      range = new int[] {iso2.start,iso1.start};
+	   else if (hint == 4)
+	      range = new int[] {iso1.stop,iso2.stop};
+	   else if (hint == 5)
+	      range = new int[] {iso2.start,iso1.stop};
+	   else if (hint == 3 || hint == 6)
+	      range = (iso1.direction) ? new int[] {iso1.start,iso1.stop} : new int[] {iso1.stop, iso1.start};
+      else if (hint == 7)
+	      range = new int[] {iso2.start,iso1.stop};
+      else if (hint == 8)
+	      range = new int[] {iso1.start,iso2.start};
+	   else if (hint == 10)
+	      range = new int[] {iso2.stop,iso1.stop};
+	   else if (hint == 11)
+	      range = new int[] {iso1.start,iso2.stop};
+	   else if (hint == 9 || hint == 12)
+	      range = (iso2.direction) ? new int[] {iso2.start,iso2.stop} : new int[] {iso2.stop, iso2.start};
+	   else
+	      range = new int[] {0,0};
+	      
+      return range;
+	}
+	
 //	intergenic region: sort list by smallest index (either start or stop)
 }
 
