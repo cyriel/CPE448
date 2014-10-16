e01f96e74497a6a2260c00e747795acd71f098ab
Elton Yang <eyang03@calpoly.edu>
2014-10-16 14:57:08 -0700

    created main function and fixed various issues

--- Gene Density/src/GeneDensity.java
+++ Gene Density/src/GeneDensity.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 /*
  * 1 class per isoform (cut off everything after dash)
@@ -15,13 +16,118 @@ column 10: name (use function to clean "" and ""; && everything after dash)
  */
 
 public class GeneDensity {
-   ArrayList<Isoform> isoforms = new ArrayList<Isoform>(); //place isoform objects here
-   ArrayList<DNAFrag> nests = new ArrayList<DNAFrag>(); // place discovered nests here
-   int length, numA, numT, numG, numC, numGC, numN;
-   int nestedCount=0, nestedLength=0, nestedA, nestedT, nestedG, nestedC, nestedGC, nestedN;
-   int igCount=0, igLength=0, igA, igT, igG, igC, igGC, igN;
-   boolean isoformSorted = false;
-   String DNA;
+   static ArrayList<Isoform> isoforms = new ArrayList<Isoform>(); //place isoform objects here
+   static ArrayList<DNAFrag> nests = new ArrayList<DNAFrag>(); // place discovered nests here
+   static int length, numA, numT, numG, numC, numGC, numN;
+   static int nestedCount=0, nestedLength=0, nestedA, nestedT, nestedG, nestedC, nestedGC, nestedN;
+   static int igCount=0, igLength=0, igA, igT, igG, igC, igGC, igN;
+   static boolean isoformSorted = false;
+   static String DNA;
+   
+   // need to implement a startOffset and an endOffset to handle searching through the GFF file for applicable ranges
+   // this /could/ still allow upstream to be calculated at full distances
+   public static void main(String[] args) {
+      int start = 1, upstream = 0, end = -1;    // default start, stop, and upstream values
+      String config = "config.txt";
+      Scanner parser;
+      try {
+         parser = new Scanner(new File(config));
+         String fastafile = parser.nextLine();
+         String gfffile = parser.nextLine();
+         if (parser.hasNextInt()) {
+            upstream = parser.nextInt();
+            if (parser.hasNextInt()) {
+               start = parser.nextInt();
+               if (parser.hasNextInt())
+                  end = parser.nextInt();
+               else
+                  start = 1;
+            }
+         }
+         start--;
+         try {
+            Scanner fasta = new Scanner(new File(fastafile));
+            fasta.nextLine();    // dump that junk line in the fasta file
+            DNA = "";     // initialize dna string
+            while (fasta.hasNextLine())
+               DNA += fasta.nextLine();
+            if (end == -1)
+               end = DNA.length();
+            String dna_fragment = DNA.substring(start,end);
+            
+            Scanner gff = new Scanner(new File(gfffile));
+            /*
+             * 1 class per isoform (cut off everything after dash)
+             *store list of isoform in arraylist
+             *
+             *Class Name: isoform
+             *Class Name: record
+             *column 3: feature
+             *column 4: start
+             *column 5: stop
+             *column 7: strand (boolean)
+             *column 10: name (use function to clean "" and ""; && everything after dash)
+             * 
+             */
+            Isoform isoform = new Isoform();
+            while (gff.hasNextLine()) {
+               String tempGFF = gff.nextLine();
+               parser = new Scanner(tempGFF);
+               parser.next();
+               parser.next();
+               String feature = parser.next();
+               int startRec = parser.nextInt();
+               int stopRec = parser.nextInt();
+               parser.next();
+               boolean direction = parser.next().charAt(0) == '+' ? true: false;
+               parser.next();
+               parser.next();
+               String isoName = parser.next();
+               isoName = isoName.substring(2,isoName.indexOf('-'));
+               
+               if (isoform.name == "")    // initialization of isoform object
+                  isoform.name = isoName;
+               else if (!isoform.name.equals(isoName)) { //add current isoform to ArrayList and create new isoform
+                  isoform.runAllDataCalc(extractGeneDNA(DNA,isoform));
+                  isoforms.add(isoform);
+                  isoform = new Isoform();
+                  isoform.name = isoName;
+               }
+               // there is no else needed because the current isoform is still current (tautological?)
+               
+               Record record = new Record(feature, startRec, stopRec, direction, isoName);
+               record.genData(extractRecordDNA(DNA,record));
+               
+               if (record.feature.equals("start_codon"))
+                  isoform.start = startRec;
+               else if (record.feature.equals("stop_codon"))
+                  isoform.stop = stopRec;
+               else if (record.feature.equals("exon"))       // not really necessary, but don't want to chance a random feature name from getting input
+                  isoform.exons.add(record);
+               isoform.direction = direction;
+               isoform.records.add(record);
+            }
+            isoform.runAllDataCalc(DNA);
+            isoforms.add(isoform); // after we finish parsing GFF file, we should still have one isoform left to add to array list
+            
+            genData(DNA);
+            calcNests();
+            calcIntergenic();
+            
+            // everything should be done calculating at this point
+            // we need to output the data to a nice CSV (probably multiple to keep things organized)
+            // possible CSV files: RecordData, GeneData
+            // possible txt file: NestedGenes
+
+            
+         } catch (FileNotFoundException e) {
+            System.err.println("Input file not found");
+         }
+      } catch (FileNotFoundException e) {
+         System.err.println("Config file not found");
+      }
+      
+   }
 
    // put this into Record's genData function to get statistical calculations
    public static String extractRecordDNA(String dna, Record rec) {
@@ -68,7 +174,7 @@ public class GeneDensity {
     *               11: nested from iso1.start to iso2.stop
     *               12: iso2 is nested inside iso1
     */
-   public int isNested(Isoform iso1, Isoform iso2) {
+   public static int isNested(Isoform iso1, Isoform iso2) {
       if (iso1.start > iso2.start && iso1.start < iso2.stop)
          if (iso1.stop > iso2.stop)                   // nested from iso1.start to iso2.stop
             return 1;
@@ -103,7 +209,7 @@ public class GeneDensity {
    
    // hint should be return value from isNested
    // return is [start,stop]
-   public int[] nested(Isoform iso1, Isoform iso2, int hint) {
+   public static int[] nested(Isoform iso1, Isoform iso2, int hint) {
       int[] range;
       if (hint == 1)
          range = new int[] {iso1.start,iso2.stop};
@@ -133,7 +239,7 @@ public class GeneDensity {
    
    // do an n^2 comparison of isoforms and find nested regions and store it as a DNAFrag in nests
    // finds and calculates Nests/data
-   public void calcNests() {
+   public static void calcNests() {
       // for each nested region, create a DNAFrag object, calculate the data, and then store it in the array list
       for (int i = 0; i < isoforms.size()-1; i++) {
          for (int n = 1; n < isoforms.size(); n++) {
@@ -149,7 +255,7 @@ public class GeneDensity {
    }
    
    // we can't generate nested and intergenic at the same time in the /veryveryvery/ rare case where we have multiple nests involving the same few genes
-   public void calcIntergenic() {
+   public static void calcIntergenic() {
       // need to sort Isoforms from lowest to highest and look at the gaps between neighbors.
       // this function is going to pretend that overlaps don't happen, so stop is between another's start and stop, we ignore it.
       // ignore the previous line....if there is an overlap, we would be able to fix that by adding in the nested regions
@@ -165,24 +271,24 @@ public class GeneDensity {
       igN = numN;
       igCount = isoforms.size()+1; // there is a good chance that this logic is correct. if we really want to be accurate and test all cases, we should have two booleans in the main to verify start and stop location
 
-      for(Records exon: records) {
-         intronLength -= exon.length;
-         intronA -= exon.numA;
-         intronT -= exon.numT;
-         intronG -= exon.numG;
-         intronC -= exon.numC;
-         intronGC -= exon.numGC;
-         intronN -= exon.numN;
+      for(Isoform iso: isoforms) {
+         igLength -= iso.length;
+         igA -= iso.numA;
+         igT -= iso.numT;
+         igG -= iso.numG;
+         igC -= iso.numC;
+         igGC -= iso.numGC;
+         igN -= iso.numN;
       }
       
    }
    
-   public void sortIsoforms() {
-      // some sorting happens here
+   public static void sortIsoforms() {
+      Collections.sort(isoforms);
       isoformSorted = true;
    }
    
-   public void genData(String dna) {
+   public static void genData(String dna) {
       length = dna.length();
       numA = length - dna.replace("A", "").length();
       numT = length - dna.replace("T", "").length();
@@ -194,9 +300,10 @@ public class GeneDensity {
    
 }
 
-class Isoform {//implements Comparable{
+class Isoform implements Comparable<Isoform>{
    ArrayList<Record> records = new ArrayList<Record>();
    ArrayList<Record> exons = new ArrayList<Record>();
+   String name = "";
    int start;
    int stop;
    boolean direction, exonSorted = false;
@@ -204,22 +311,17 @@ class Isoform {//implements Comparable{
    int intronCount, intronLength, intronA, intronT, intronG, intronC, intronGC, intronN;
    int CDScount=0, CDSlength=0, exonCount=0, exonLength=0;
    
-   public void setStart(int start) {
-      this.start = start;
-   }
-   
-   public void setStop(int stop) {
-      this.stop = stop;
-   }
-   
-   public void setDirection(boolean Direction) {
-      this.direction = direction;
-   }
-   
    public void runAllDataCalc(String dna) {
       genData(dna);
       calcIntronData();
       genRecordStat();
+   }
+   
+   public int compareTo(Isoform other) {
+      if (start == other.start)
+         return (stop == other.stop) ? 0 : stop - other.stop;
+      else
+         return start - other.start;
    }
    
    public void calcIntronData() {
@@ -235,7 +337,7 @@ class Isoform {//implements Comparable{
       intronN = numN;
       intronCount = exons.size()-1;
 
-      for(Records exon: records) {
+      for(Record exon: records) {
          intronLength -= exon.length;
          intronA -= exon.numA;
          intronT -= exon.numT;
@@ -271,14 +373,13 @@ class Isoform {//implements Comparable{
    }
    
    public void sortExons() {
-      // some sorting happends here.
-      // probably Collections.sort()
+      Collections.sort(records);
       
       exonSorted = true;
    }
 }
 
-class Record {//implements Comparable{
+class Record implements Comparable<Record>{
    String feature, isoform;
    int start, stop;
    boolean direction; // true = forward, false = backwards
@@ -300,6 +401,13 @@ class Record {//implements Comparable{
          type = 3;
       else if (feature.equals("exon"))
          type = 4;
+   }
+   
+   public int compareTo(Record other) {
+      if (start == other.start)
+         return (stop == other.stop) ? 0 : stop - other.stop;
+      else
+         return start - other.start;
    }
    
    // assume that the dna string is pre-parsed using extractRecordDNA()
